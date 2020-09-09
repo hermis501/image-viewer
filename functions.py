0475bd60da6993cbe52d3d8ce3400591fc110b68
Hermis <hermis.kasperavicius@gmail.com>
2020-09-09 22:42:10 +0300

    updated

--- functions.py
+++ functions.py
@@ -1,10 +1,10 @@
 import os
-def search_files (dir, ext):
+def search_photos (dir):
 	aarray=[]
 	aarray.append (0)
 	allDirs=os.listdir (dir)
 		for i in range (len(allDirs):
-			if not os.path.isdir (allDirs[i]) and get_extension (allDirs[i])==ext: aarray.append (allDirs[i])
+			if not os.path.isdir (allDirs[i]) and get_extension (allDirs[i])=="jpg" or get_extension (allDirs[i])=="png": aarray.append (allDirs[i])
 	aarray[0]=len(aarray)
 	return aarray
 
